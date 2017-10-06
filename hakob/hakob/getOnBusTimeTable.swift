c4b7f0cafb0f195c715e8fd22ab4e58fcfd99275
stb1015157 <b1015157@fun.ac.jp>
2017-10-06 17:42:57 +0900

    no message

---
+++ hakob/hakob/getOnBusTimeTable.swift
@@ -0,0 +1,37 @@
+//
+//  getOnBusTimeTable.swift
+//  hakob
+//
+//  Created by stb1015157 on 2017/10/06.
+//  Copyright © 2017年 佐藤秀輔. All rights reserved.
+//
+
+import UIKit
+
+class getOnBusTimeTable: UIViewController {
+
+    @IBOutlet weak var getOnBusLine: UINavigationBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getOnBusLine.
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
