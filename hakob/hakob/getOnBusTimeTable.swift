e5f5c679df79d913975515ad98151a0c6d1edaf9
stb1015157 <b1015157@fun.ac.jp>
2017-10-18 18:01:01 +0900

    10/18作業分 途中 エラーあり

--- hakob/hakob/getOnBusTimeTable.swift
+++ hakob/hakob/getOnBusTimeTable.swift
@@ -12,7 +12,15 @@ class getOnBusTimeTable: UIViewController, UITableViewDelegate, UITableViewDataS
     var destination = ["函館駅", "五稜郭", "亀田支所", "はこだて未来大学"]
     var busStop = ["函館駅前5番のりば", "五稜郭", "富岡", "亀田支所前", "はこだて未来大学"]
     var delayTime = 5
+/*
     var stopTime = ["8:03", "8:09", "8:40", "9:14", "9:28", "10:11", "10:16", "10:34", "11:04", "11:11", "11:49", "12:12", "13:12", "13:20", "13:45", "14:58", "15:27", "16:10", "17:09", "17:34", "18:21", "18:54", "19:28", "20:35"]
+ */
+    var stopTime = [(8*60+3), (8*60+9), (8*60+40), (9*60+14), (9*60+28), (10*60+11), (10*60+16), (10*60+34), (11*60+4), (11*60+11), (11*60+49), (12*60+12), (13*60+12), (13*60+20), (13*60+45), (14*60+58), (15*60+27), (16*60+10), (17*60+9), (17*60+34), (18*60+21), (18*60+54), (19*60+28), (20*60+35)]
+    let now = NSDate()
+    let cal = NSCalendar.current
+    let comp = cal.components(
+        [NSCalendar.Unit.Year, NSCalendar.Unit.Month, NSCalendar.Unit.Day,
+         NSCalendar.Unit.Hour, NSCalendar.Unit.Minute, NSCalendar.Unit.Second], fromDate: now)
     
     @IBOutlet weak var titleBusStop: UINavigationBar!
     @IBOutlet weak var destinationLabel: UILabel!
@@ -21,7 +29,10 @@ class getOnBusTimeTable: UIViewController, UITableViewDelegate, UITableViewDataS
     @IBOutlet weak var getOffBusStop: UILabel!
     @IBOutlet weak var delayTimeList: UITableView!
     @IBOutlet weak var getOnBusStop2: UILabel!
-    
+    @IBOutlet weak var cellLabel1: UILabel!
+    @IBOutlet weak var cellLabel2: UILabel!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -30,6 +41,9 @@ class getOnBusTimeTable: UIViewController, UITableViewDelegate, UITableViewDataS
         getOffBusStop.text = busStop[4]
         titleBusStop.accessibilityLabel = getOnBusStop.text
         getOnBusStop2.text = getOnBusStop.text!
+        var delayTimes = [1]
+        delayTimes[0] = now - stopTime[0]
+        print(delayTimes[0])
     }
 
     override func didReceiveMemoryWarning() {
@@ -54,8 +68,18 @@ class getOnBusTimeTable: UIViewController, UITableViewDelegate, UITableViewDataS
         let cell: UITableViewCell =
             tableView.dequeueReusableCell(withIdentifier:
                 "delayTime", for: indexPath)
+        
+        // Tagが1の部品にアクセス
+        cellLabel1 = cell.viewWithTag(1) as! UILabel
+        
+        // Tagが2の部品にアクセス
+        cellLabel2 = cell.viewWithTag(2) as! UILabel
+        cellLabel2.text = "あと" + String(delayTime) + "分で到着"
+        
         // セルに表示する値を設定する
+/*
         cell.textLabel!.text = stopTime[indexPath.row]
+ */
         return cell
     }
     
@@ -64,7 +88,7 @@ class getOnBusTimeTable: UIViewController, UITableViewDelegate, UITableViewDataS
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: IndexPath) {
         print("セル番号：¥(indexPath.row)　セルの内容：¥(stopTime[indexPath.row])")
     }
-*/
+ */
 
 /* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */
 
