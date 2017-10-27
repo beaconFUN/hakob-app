d8bda2a89f6b4fb30ce6c55d0bab99a2b6b9d43e
stb1015157 <b1015157@fun.ac.jp>
2017-10-27 17:59:36 +0900

    亀田支所の上り時刻表しか入れてなかったのを、亀田支所以降未来大までのバス停の上り時刻表も表示出来るようにした。

--- hakob/hakob/getOnBusTimeTable.swift
+++ hakob/hakob/getOnBusTimeTable.swift
@@ -11,13 +11,17 @@ import Foundation
 
 class getOnBusTimeTable: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var destination = ["函館駅", "五稜郭", "亀田支所", "はこだて未来大学"]
-    var busStop = ["函館駅前5番のりば", "松風町(1)プレイガイド前", "松風町(2)セブンイレブン前", "新川町", "千歳町", "昭和橋", "堀川町", "千代台", "中央病院前", "五稜郭(1)シダックス前", "五稜郭公園入口", "警察署前警察署向かい", "田家入口", "医師会病院前", "富岡", "亀田支所前(1)至赤川", "函館地方気象台前", "赤川通", "赤川1丁目ライフプレステージ白ゆり美原前", "赤川入口", "低区貯水池", "浄水場下", "赤川小学校", "赤川3区", "赤川貯水池", "はこだて未来大学", "赤川4区", "下赤川", "赤川"]
+    var busStop = ["亀田支所前(1)至赤川", "函館地方気象台前", "赤川通", "赤川1丁目ライフプレステージ白ゆり美原前", "赤川入口", "低区貯水池", "浄水場下", "赤川小学校", "赤川3区", "赤川貯水池", "はこだて未来大学"]
 /*
-    var stopTime = ["8:03", "8:09", "8:40", "9:14", "9:28", "10:11", "10:16", "10:34", "11:04", "11:11", "11:49", "12:12", "13:12", "13:20", "13:45", "14:58", "15:27", "16:10", "17:09", "17:34", "18:21", "18:54", "19:28", "20:35"]
- */
-    let stopTime = [8*60+3, 8*60+9, 8*60+40, 9*60+14, 9*60+28, 10*60+11, 10*60+16, 10*60+34, 11*60+4, 11*60+11, 11*60+49, 12*60+12, 13*60+12, 13*60+20, 13*60+45, 14*60+58, 15*60+27, 16*60+10, 17*60+9, 17*60+34, 18*60+21, 18*60+54, 19*60+28, 20*60+35]
+    var stopTimeKameda105Nobori = ["8:03", "8:09", "8:40", "9:14", "9:28", "10:11", "10:16", "10:34", "11:04", "11:11", "11:49", "12:12", "13:12", "13:20", "13:45", "14:58", "15:27", "16:10", "17:09", "17:34", "18:21", "18:54", "19:28", "20:35"]
+     */
+    let stopTimeKameda105Nobori = [8*60+3, 8*60+9, 8*60+40, 9*60+14, 9*60+28, 10*60+11, 10*60+16, 10*60+34, 11*60+4, 11*60+11, 11*60+49, 12*60+12, 13*60+12, 13*60+20, 13*60+45, 14*60+58, 15*60+27, 16*60+10, 17*60+9, 17*60+34, 18*60+21, 18*60+54, 19*60+28, 20*60+35] // 亀田支所の105系統上りの時刻表
+    let stopTimeKameda105Kudari = [8*60+15, 8*60+21, 8*60+52, 9*60+26, 9*60+40, 10*60+23, 10*60+28, 10*60+46, 11*60+16, 11*60+23, 12*60+1, 13*60+24, 13*60+24, 13*60+32, 13*60+57, 15*60+10, 15*60+39, 16*60+22, 17*60+21, 17*60+46, 18*60+33, 19*60+6, 19*60+40, 20*60+47] // 亀田支所の105系統下りの時刻表
+    var busStopsTimeN = [0]
+    var busStopsTimeK = [0]
+    let timeFromKameda = [2, 3, 4, 5, 6, 7, 8, 9, 10, 12] // 亀田支所からそれ以降の各バス停までかかる時間 [気象台, 赤川通, 赤川1丁目, 赤川入口, 低区貯水池, 浄水場下, 赤川小学校, 赤川3区, 赤川貯水池, はこだて未来大学]
     var delayTime = [0]
-    var cellNum = 0
+    var cellNum = 0 // 現在時刻以前に来るバスの数
     var num = 0
     
     
@@ -35,22 +39,124 @@ class getOnBusTimeTable: UIViewController, UITableViewDelegate, UITableViewDataS
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        /* ━━━━━━━━━━━━━━━━━━━━━━━━━━━現在時刻をint型で取得━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */
         let now = Date()
         let cal: Calendar = Calendar(identifier: .gregorian)
         let cal_comp: DateComponents = cal.dateComponents([.hour, .minute], from:now)
-        
+
+        /* ━━━━━━━━━━━━━━━━━━━━━━━━━目的地、乗車地、降車地を設定━━━━━━━━━━━━━━━━━━━━━━━━━ */
         destinationLabel.text = "目的地：" + destination[0]
-        getOnBusStop.text = busStop[0]
+        getOnBusStop.text = busStop[8]
         getOffBusStop.text = busStop[4]
         titleBusStop.accessibilityLabel = getOnBusStop.text
         getOnBusStop2.text = getOnBusStop.text!
-        /* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */
-        delayTime = [Int](repeating: 0, count: stopTime.count)
-        for i in 0 ..< stopTime.count {
-        delayTime[i] = stopTime[i] - (cal_comp.hour!*60 + cal_comp.minute!)
+        
+        /* ━━━━━━━━━━━━━━━━━━━━━━━━━━亀田支所以降のバス停の時刻表━━━━━━━━━━━━━━━━━━━━━━━━━ */
+        busStopsTimeN = [Int](repeating: 0, count: stopTimeKameda105Nobori.count)
+        busStopsTimeK = [Int](repeating: 0, count: stopTimeKameda105Nobori.count)
+        // 上り
+        switch getOnBusStop.text! {
+        case busStop[1]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeN[i] = stopTimeKameda105Nobori[i] + timeFromKameda[0]
+            }
+        case busStop[2]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeN[i] = stopTimeKameda105Nobori[i] + timeFromKameda[1]
+            }
+        case busStop[3]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeN[i] = stopTimeKameda105Nobori[i] + timeFromKameda[2]
+            }
+        case busStop[4]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeN[i] = stopTimeKameda105Nobori[i] + timeFromKameda[3]
+            }
+        case busStop[5]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeN[i] = stopTimeKameda105Nobori[i] + timeFromKameda[4]
+            }
+        case busStop[6]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeN[i] = stopTimeKameda105Nobori[i] + timeFromKameda[5]
+            }
+        case busStop[7]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeN[i] = stopTimeKameda105Nobori[i] + timeFromKameda[6]
+            }
+        case busStop[8]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeN[i] = stopTimeKameda105Nobori[i] + timeFromKameda[7]
+            }
+        case busStop[9]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeN[i] = stopTimeKameda105Nobori[i] + timeFromKameda[8]
+            }
+        case busStop[10]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeN[i] = stopTimeKameda105Nobori[i] + timeFromKameda[9]
+            }
+        default:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeK[i] = stopTimeKameda105Nobori[i]
+            }
         }
-        /* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */
-        num = cal_comp.hour!
+        // 下り
+        switch getOnBusStop.text! {
+        case busStop[1]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeK[i] = stopTimeKameda105Kudari[i] + timeFromKameda[0]
+            }
+        case busStop[2]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeK[i] = stopTimeKameda105Kudari[i] + timeFromKameda[1]
+            }
+        case busStop[3]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeK[i] = stopTimeKameda105Kudari[i] + timeFromKameda[2]
+            }
+        case busStop[4]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeK[i] = stopTimeKameda105Kudari[i] + timeFromKameda[3]
+            }
+        case busStop[5]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeK[i] = stopTimeKameda105Kudari[i] + timeFromKameda[4]
+            }
+        case busStop[6]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeK[i] = stopTimeKameda105Kudari[i] + timeFromKameda[5]
+            }
+        case busStop[7]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeK[i] = stopTimeKameda105Kudari[i] + timeFromKameda[6]
+            }
+        case busStop[8]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeK[i] = stopTimeKameda105Kudari[i] + timeFromKameda[7]
+            }
+        case busStop[9]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeK[i] = stopTimeKameda105Kudari[i] + timeFromKameda[8]
+            }
+        case busStop[10]:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeK[i] = stopTimeKameda105Kudari[i] + timeFromKameda[9]
+            }
+        default:
+            for i in 0 ..< stopTimeKameda105Nobori.count {
+                busStopsTimeK[i] = stopTimeKameda105Kudari[i]
+            }
+        }
+
+        /* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━あと何分で到着するか━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */
+        delayTime = [Int](repeating: 0, count: stopTimeKameda105Nobori.count)
+        for i in 0 ..< stopTimeKameda105Nobori.count {
+        delayTime[i] = stopTimeKameda105Nobori[i] - (cal_comp.hour!*60 + cal_comp.minute!)
+        }
+
+        /* ━━━━━━━━━━━━━━━━━━━━━━━現在時刻以前に来るバスの数を数える━━━━━━━━━━━━━━━━━━━━━━ */
+        // num = cal_comp.hour!
         for i in 0 ..< delayTime.count {
             if delayTime[i] < 0 {
                 cellNum += 1
@@ -64,13 +170,13 @@ class getOnBusTimeTable: UIViewController, UITableViewDelegate, UITableViewDataS
     }
 
 
-/* ━━━━━━━━━━━━━━━━━━━━━━━セルの設定━━━━━━━━━━━━━━━━━━━━━━━━ */
+/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━セルの設定━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */
 
     // セルの個数を指定するデリゲートメソッド(必須)
     func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
         // セルの数を設定
-        return stopTime.count - cellNum// stopTime.count
+        return stopTimeKameda105Nobori.count - cellNum// stopTimeKameda105Nobori.count
     }
     
     // セルに値を設定するデータメソッド(必須)
@@ -85,15 +191,16 @@ class getOnBusTimeTable: UIViewController, UITableViewDelegate, UITableViewDataS
         /*
         // Tagが1の部品にアクセス
         cellLabel1 = cell.viewWithTag(1) as! UILabel
-            cellLabel1.text = String(stopTime[indexPath.row]/60) + ":" + String(format: "%02d", stopTime[indexPath.row]%60)
+            cellLabel1.text = String(stopTimeKameda105Nobori[indexPath.row]/60) + ":" + String(format: "%02d", stopTimeKameda105Nobori[indexPath.row]%60)
         
         // Tagが2の部品にアクセス
         cellLabel2 = cell.viewWithTag(2) as! UILabel
         cellLabel2.text = "あと" + String(delayTime[indexPath.row]) + "分で到着"
          */
+
         // Tagが1の部品にアクセス
             cellLabel1 = cell.viewWithTag(1) as! UILabel
-            cellLabel1.text = String(stopTime[cellNum]/60) + ":" + String(format: "%02d", stopTime[cellNum]%60)
+            cellLabel1.text = String(busStopsTimeN[cellNum]/60) + ":" + String(format: "%02d", busStopsTimeN[cellNum]%60)
             
         // Tagが2の部品にアクセス
             cellLabel2 = cell.viewWithTag(2) as! UILabel
@@ -104,12 +211,12 @@ class getOnBusTimeTable: UIViewController, UITableViewDelegate, UITableViewDataS
     
     /// セルが選択された時に呼ばれるデリゲートメソッド
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("セル番号：¥(indexPath.row)　セルの内容：¥(stopTime[indexPath.row])")
+        print("セル番号：¥(indexPath.row)　セルの内容：¥(stopTimeKameda105Nobori[indexPath.row])")
         performSegue(withIdentifier: "toBusLine", sender: indexPath.row)
         tableView.deselectRow(at: tableView.indexPathForSelectedRow!, animated: true)
     }
 
-/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */
+/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
